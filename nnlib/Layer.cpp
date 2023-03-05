25aab7dca5c20aa81cdc78aab1c3268b5a316e62
BJ Anderson <80845362+Auric115@users.noreply.github.com>
2023-03-04 22:34:58 -0600

    Constructors & Helper Functions

--- nnlib/Layer.cpp
+++ nnlib/Layer.cpp
@@ -1,1 +1,12 @@
 #include "nnlib.h"
+
+double squish(double x) { return std::tanh(x); }
+double dSquish(double x) { return (1.0 - (x * x)); }
+
+double init_weight() { return ((double)rand()) / ((double)RAND_MAX); }
+
+Layer::Layer(unsigned nodes, double lr)
+{
+    numNodes_m_u = nodes;
+    lr_m_d = lr;
+}
--- nnlib/NeuralNetwork.cpp
+++ nnlib/NeuralNetwork.cpp
@@ -1,1 +1,8 @@
 #include "nnlib.h"
+
+NeuralNetwork::NeuralNetwork(unsigned inodes, double lr)
+{
+    topology_m_uA[numLayers_m_u] = inodes;
+    layers_m_cpA[numLayers_m_u++] = new Layer(inodes, lr);
+    learningRate_m_d = lr;
+}

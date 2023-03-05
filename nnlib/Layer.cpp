ea535a7733cfb22d2490f466ca804d8ca6f32420
BJ Anderson <80845362+Auric115@users.noreply.github.com>
2023-03-04 22:36:37 -0600

    Display, AddLayer, & Create Mesh Methods

--- nnlib/Layer.cpp
+++ nnlib/Layer.cpp
@@ -5,6 +5,55 @@ double dSquish(double x) { return (1.0 - (x * x)); }
 
 double init_weight() { return ((double)rand()) / ((double)RAND_MAX); }
 
+void Layer::CreateMesh(unsigned outNodes)
+{
+    outNodes_m_u = outNodes;
+    for (unsigned oc = 0; oc < outNodes_m_u; oc++)
+    {
+        biases_m_dA[oc] = init_weight();
+        for (unsigned ic = 0; ic < numNodes_m_u; ic++)
+        {
+            weights_m_d2A[ic][oc] = init_weight();
+        }
+    }
+}
+
+void Layer::Display()
+{
+    std::cout << std::setw(12) << "Nodes: " << numNodes_m_u << std::endl;
+
+    std::cout << std::setw(12) << "Intakes: ";
+    for (unsigned n = 0; n < numNodes_m_u; n++)
+    {
+        std::cout << intakes_dA[n] << ' ';
+    }
+    std::cout << std::endl;
+
+    if (outNodes_m_u > 0)
+    {
+        std::cout << std::setw(12) << "Gradient: ";
+        for (unsigned n = 0; n < outNodes_m_u; n++)
+        {
+            std::cout << gradients_dA[n] << ' ';
+        }
+        std::cout << std::endl;
+
+        std::cout << std::endl;
+        for (unsigned ic = 0; ic < numNodes_m_u; ic++)
+        {
+            for (unsigned oc = 0; oc < outNodes_m_u; oc++)
+            {
+                std::cout << "\tW" << oc << ": " << weights_m_d2A[ic][oc] << std::endl;
+            }
+            std::cout << std::endl;
+        }
+    }
+    for (unsigned oc = 0; oc < outNodes_m_u; oc++)
+    {
+        std::cout << "\tB" << oc << ": " << biases_m_dA[oc] << std::endl;
+    }
+}
+
 Layer::Layer(unsigned nodes, double lr)
 {
     numNodes_m_u = nodes;
--- nnlib/NeuralNetwork.cpp
+++ nnlib/NeuralNetwork.cpp
@@ -1,5 +1,48 @@
 #include "nnlib.h"
 
+void NeuralNetwork::AddLayer(unsigned nodes)
+{
+    topology_m_uA[numLayers_m_u] = nodes;
+    layers_m_cpA[numLayers_m_u - 1]->CreateMesh(nodes);
+    layers_m_cpA[numLayers_m_u] = new Layer(nodes, learningRate_m_d);
+    layers_m_cpA[numLayers_m_u]->prevLayer_pC = layers_m_cpA[numLayers_m_u - 1];
+    numLayers_m_u++;
+}
+
+void NeuralNetwork::Display()
+{
+    std::cout << "------------ Neural Network ------------" << std::fixed << std::setprecision(4) << std::endl;
+    std::cout << std::setw(12) << "Topology: ";
+    for (unsigned l = 0; l < numLayers_m_u; l++)
+    {
+        std::cout << topology_m_uA[l] << std::setw(MAXNODES / 10 + 3);
+    }
+    std::cout << std::endl;
+    std::cout << std::setw(12) << "Learning: " << learningRate_m_d << std::setw(MAXNODES / 10 + 8) << std::endl;
+
+    std::cout << std::setw(12) << "Results: ";
+    for (unsigned o = 0; o < topology_m_uA[numLayers_m_u - 1]; o++)
+    {
+        std::cout << results_m_dA[o] << std::setw(MAXNODES / 10 + 8);
+    }
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    for (unsigned l = 0; l < numLayers_m_u; l++)
+    {
+        std::cout << "  ------------- Layer: " << l << " -------------  " << std::endl;
+        layers_m_cpA[l]->Display();
+        std::cout << std::endl;
+    }
+    std::cout << std::setw(12) << "Outputs: ";
+    for (unsigned o = 0; o < topology_m_uA[numLayers_m_u - 1]; o++)
+    {
+        std::cout << outputs_m_dA[o] << ' ';
+    }
+    std::cout << std::endl;
+    std::cout << std::endl;
+}
+
 NeuralNetwork::NeuralNetwork(unsigned inodes, double lr)
 {
     topology_m_uA[numLayers_m_u] = inodes;

7aade1886bbf83c8048301ce61214086f85f88f1
BJ Anderson <80845362+Auric115@users.noreply.github.com>
2023-03-22 14:41:25 -0500

    Updating IOManip in Display

--- nnlib/NeuralNetwork.cpp
+++ nnlib/NeuralNetwork.cpp
@@ -99,28 +99,28 @@ void NeuralNetwork::Display(bool displayLayers /*= true*/, int precision /*= 6*/
     std::cout << std::setw(12) << "Topology: ";
     for (unsigned l = 0; l < numLayers_m_u; l++)
     {
-        std::cout << topology_m_uA[l] << std::setw(MAXNODES / 10 + 3);
+        std::cout << topology_m_uA[l] << std::setw(precision / 2 + 3);
     }
     std::cout << std::endl;
-    std::cout << std::setw(12) << "Learning: " << learningRate_m_d << std::setw(MAXNODES / 10 + 8) << std::endl;
-    std::cout << std::setw(12) << "RA Error: " << ((timesTrained_m_u > 0) ? (totalError_m_d / (double)timesTrained_m_u) : (0.0)) << std::setw(MAXNODES / 10 + 8) << std::endl;
+    std::cout << std::setw(12) << "Learning: " << learningRate_m_d << std::setw(precision / 2 + 8) << std::endl;
+    std::cout << std::setw(12) << "RA Error: " << ((timesTrained_m_u > 0) ? (totalError_m_d / (double)timesTrained_m_u) : (0.0)) << std::setw(precision / 2 + 8) << std::endl;
     std::cout << std::endl;
     std::cout << std::setw(12) << "Inputs: ";
     for (unsigned i = 0; i < topology_m_uA[0]; i++)
     {
-        std::cout << inputs_m_dA[i] << ' ' << std::setw(MAXNODES / 10 + 8);
+        std::cout << inputs_m_dA[i] << ' ' << std::setw(precision / 2 + 8);
     }
     std::cout << std::endl;
     std::cout << std::setw(12) << "Outputs: ";
     for (unsigned o = 0; o < topology_m_uA[numLayers_m_u - 1]; o++)
     {
-        std::cout << outputs_m_dA[o] << ' ' << std::setw(MAXNODES / 10 + 8);
+        std::cout << outputs_m_dA[o] << ' ' << std::setw(precision / 2 + 8);
     }
     std::cout << std::endl;
     std::cout << std::setw(12) << "Results: ";
     for (unsigned r = 0; r < topology_m_uA[numLayers_m_u - 1]; r++)
     {
-        std::cout << results_m_dA[r] << ' ' << std::setw(MAXNODES / 10 + 8);
+        std::cout << results_m_dA[r] << ' ' << std::setw(precision / 2 + 8);
     }
     std::cout << std::endl;
     std::cout << std::endl;
